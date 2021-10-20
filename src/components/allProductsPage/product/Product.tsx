d32c27fe77649866884dab5d043634f9edc814a7
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-10-20 10:50:07 +0300

    added readme

--- src/components/allProductsPage/product/Product.tsx
+++ src/components/allProductsPage/product/Product.tsx
@@ -3,12 +3,13 @@ import { useEffect } from "react";
 import db from "../../../firebase";
 
 const Product = () => {
-  useEffect(() => {
-    const unsub = onSnapshot(collection(db, "momos_nv"), (snapshot) => {
-      snapshot.docs.map((doc) => doc.data());
-    });
-    return unsub;
-  }, []);
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "momos_nv"), (snapshot) => {
+        snapshot.docs.map((doc) => doc.data());
+      }),
+    []
+  );
   return <div>helllooo</div>;
 };
 
--- src/types/allProductsType.ts
+++ src/types/allProductsType.ts
@@ -15,28 +15,9 @@ export interface FruitsType {
 }
 export enum AllProductsActionTypes {
   FETCH_ALL_PRODUCTS = "FETCH_ALL_PRODUCTS",
-  FETCH_ALL_PRODUCTS_SUCCESS = "FETCH_ALL_PRODUCTS_SUCCESS",
-  FETCH_ALL_PRODUCTS_ERROR = "FETCH_ALL_PRODUCTS_ERROR",
-  FETCH_ALL_PRODUCTS_PAGE = "FETCH_ALL_PRODUCTS_PAGE",
 }
 export interface FetchAllProductsAction {
   type: AllProductsActionTypes.FETCH_ALL_PRODUCTS;
 }
-export interface FetchAllProductsSuccessAction {
-  type: AllProductsActionTypes.FETCH_ALL_PRODUCTS_SUCCESS;
-  payload: any;
-}
-export interface FetchAllProductsErrorAction {
-  type: AllProductsActionTypes.FETCH_ALL_PRODUCTS_ERROR;
-  payload: string;
-}
-export interface FetchAllProductsPageAction {
-  type: AllProductsActionTypes.FETCH_ALL_PRODUCTS_PAGE;
-  payload: number;
-}
 
-export type AllProductsAction =
-  | FetchAllProductsAction
-  | FetchAllProductsSuccessAction
-  | FetchAllProductsErrorAction
-  | FetchAllProductsPageAction;
+export type AllProductsAction = FetchAllProductsAction;
