0bb494fcfa93c1b82bb51efd833b0e49073611b0
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-10-27 11:11:23 +0300

    added button, but the button is not working yet

--- src/components/content/pagination/Paging.tsx
+++ src/components/content/pagination/Paging.tsx
@@ -1,4 +1,6 @@
 import { Pagination } from "antd";
+import style from "./Paging.module.css";
+import productArrow from "../../../assets/images/productArrow.svg";
 
 interface Props {
   currentPage: number;
@@ -13,19 +15,31 @@ const Paging = ({
   filteredProductCount,
   setCurrentPage,
 }: Props) => {
+  const productsLeft = filteredProductCount - productPortion;
+
   const onPageChange = (page: number) => {
     setCurrentPage(page);
   };
 
   return (
-    <div>
+    <div className={style.paginationContainer}>
       <Pagination
+        className={style.pagination}
+        size="small"
         showSizeChanger={false}
         defaultCurrent={currentPage}
         total={filteredProductCount}
         defaultPageSize={productPortion}
         onChange={onPageChange}
       />
+      <button className={style.button}>
+        Show more products
+        <img className={style.arrow} src={productArrow} alt="arrow" />
+      </button>
+      <div>
+        <span className={style.productsLeft}>{productsLeft}</span>
+        <span className={style.products}>Products</span>
+      </div>
     </div>
   );
 };
