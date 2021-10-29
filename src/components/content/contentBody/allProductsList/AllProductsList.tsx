a96373641d86add595bdd93c793041a38fc15a1b
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-10-29 16:05:51 +0300

    simple reasembled mainFilter

--- src/components/content/contentBody/allProductsList/AllProductsList.tsx
+++ src/components/content/contentBody/allProductsList/AllProductsList.tsx
@@ -1,14 +1,14 @@
 import { useAppSelector } from "../../../../hooks/selectorHook";
-import cleanProduct from "./product/cleanProduct";
 import Product from "./product/Product";
 import style from "./AllProductsList.module.css";
+import mainFilter from "./product/mainFilter";
 
 const AllProductsList = () => {
   const { products, filterName, filterStars } = useAppSelector(
     (state) => state.products
   );
 
-  const filteredProducts = cleanProduct(products, filterName, filterStars)
+  const filteredProducts = mainFilter(products, filterName, filterStars)
     .slice(1, 5)
     .map((prod) => <Product prod={prod} key={prod.itemID} />);
 
