1e557567789d60e43a9d655113d882ebc0271870
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-10-29 14:58:29 +0300

    fixed issue with slider styles

---
+++ src/components/content/contentBody/sideBar/filterByPrice/sliderStyles.ts
@@ -0,0 +1,18 @@
+export const trackStyle = [{ backgroundColor: "#6A983C" }];
+
+export const handleStyle = [
+  {
+    borderColor: "#EBEBEB",
+    height: 20,
+    width: 20,
+    backgroundColor: "#FFFFFF",
+    marginTop: -6,
+  },
+  {
+    borderColor: "#EBEBEB",
+    height: 20,
+    width: 20,
+    backgroundColor: "#FFFFFF",
+    marginTop: -6,
+  },
+];
