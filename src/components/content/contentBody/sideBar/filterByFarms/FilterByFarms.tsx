c3ea69a053e35f172eb8fe81f6c05b39686b3642
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-10-31 12:11:05 +0200

    just started to map farms

--- src/components/content/contentBody/sideBar/filterByFarms/FilterByFarms.tsx
+++ src/components/content/contentBody/sideBar/filterByFarms/FilterByFarms.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useAppSelector } from "../../../../../hooks/selectorHook";
 import CheckBox from "../../../../custom/CheckBox";
 import style from "./FilterByFarms.module.css";
 
 const FilterByFarms: React.FC = () => {
-  const farms = Array(5)
-    .fill(0)
-    .map((_, i) => (
-      <li className={style.oneBox} key={i}>
-        <CheckBox />
-        <span>Filter by Farm</span>
-      </li>
-    ));
+  const { products } = useAppSelector((state) => state.products);
+
+  const allFarms = products.map((prod) => prod.farm);
+  const unicFarms = allFarms.filter(
+    (value, index, self) => self.indexOf(value) === index
+  );
+
+  const farms = unicFarms.map((el, i) => (
+    <li className={style.oneBox} key={i}>
+      <CheckBox />
+      <span>{el}</span>
+    </li>
+  ));
 
   return (
     <>
