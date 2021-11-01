fc6dabaf2c10cab876cae961cea78e6a6282c810
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-11-01 17:36:10 +0200

    added how much items are on the page

--- src/components/content/contentHeader/ContentHeader.tsx
+++ src/components/content/contentHeader/ContentHeader.tsx
@@ -2,14 +2,16 @@ import { useAppSelector } from "../../../hooks/selectorHook";
 import style from "./ContentHeader.module.css";
 
 const ContentHeader = () => {
-  const productCount = useAppSelector(
-    (state) => state.products.filteredProductCount
+  const { filteredProductCount, productPortion } = useAppSelector(
+    (state) => state.products
   );
   return (
     <div className={style.titleContainer}>
       <h1 className={style.title}>All Products</h1>
       <div>
-        <span className={style.totalCount}>{productCount}</span>
+        <span className={style.totalCount}>
+          {productPortion} / {filteredProductCount}
+        </span>
         <span className={style.products}>Products</span>
       </div>
     </div>
