4078f3780e3c87e523b434263632240157b57c90
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-11-04 22:38:03 +0200

    added something

--- src/components/content/contentBody/ContentBody.tsx
+++ src/components/content/contentBody/ContentBody.tsx
@@ -2,16 +2,35 @@ import CustomPagination from "./pagination/CustomPagination";
 import AllProductsList from "./allProductsList/AllProductsList";
 import style from "./ContentBody.module.css";
 import SideBar from "./sideBar/SideBar";
-import React from "react";
-import { Popover, Button } from "antd";
+import React, { useState } from "react";
+import { Drawer } from "antd";
+import filter from "../../../assets/images/filter.png";
 
 const ContentBody: React.FC = () => {
+  const [visible, setVisible] = useState(false);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <div className={style.mainContainer}>
       <div className={style.popOver}>
-        <Popover placement="right" content={SideBar} trigger="click">
-          <Button>Filters</Button>
-        </Popover>
+        <button className={style.button} onClick={showDrawer}>
+          <img className={style.filter} src={filter} alt="arrow" />
+          Filters
+        </button>
+        <Drawer
+          title="Filter By:"
+          placement="left"
+          onClose={onClose}
+          visible={visible}
+          width="400px"
+        >
+          <SideBar />
+        </Drawer>
       </div>
       <div className={style.container}>
         <div className={style.sideBar}>
