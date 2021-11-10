9360bebc80e9748d850c9db5c73700cbc69bcd0e
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-11-10 11:38:57 +0200

    just playing

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import style from "./Breadcrumbs.module.css";
 import { Breadcrumb } from "antd";
 import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { useHistory, useLocation, useParams } from "react-router";
 
 const Breadcrumbs: React.FC = () => {
+  const location = useParams();
+  const notLocation = useLocation();
+  const history = useHistory();
+  console.log(location);
+  console.log(notLocation);
+  console.log(history);
+
   return (
     <Breadcrumb className={style.container}>
-      <Breadcrumb.Item href="">
+      <Breadcrumb.Item href="/#/">
         <HomeOutlined />
         <span>Homepage</span>
       </Breadcrumb.Item>
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,8 +6,6 @@ export interface IRoute {
   path: string;
   component: React.ComponentType;
   exact?: boolean;
-  breadcrumbName: string;
-  children?: Array<{ path: string; breadcrumbName: string }>;
 }
 export enum RouteNames {
   ALL_PRODUCTS = "/",
@@ -20,18 +18,15 @@ export const Routes: IRoute[] = [
     path: RouteNames.ALL_PRODUCTS,
     component: AllProducts,
     exact: true,
-    breadcrumbName: "All products",
   },
   {
     path: RouteNames.PRODUCT_DESCRIPTION + "/:id",
     component: ProductDescription,
     exact: true,
-    breadcrumbName: "Homepage",
   },
   {
     path: RouteNames.SHOPPING_CART,
     component: ShippingCart,
     exact: true,
-    breadcrumbName: "Homepage",
   },
 ];
