740fcd38d8ed020792fbeb5a42d40fd4f14cdd2e
Mikhail Voloshyn <mikhailvoloshyn@gmail.com>
2021-11-28 14:35:13 +0200

    fixed firebase issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_j7yefc5tX7Dor1FHuJdswiIYJ1UbYfU",
-  authDomain: "newshopproject-da97b.firebaseapp.com",
-  projectId: "newshopproject-da97b",
-  storageBucket: "newshopproject-da97b.appspot.com",
-  messagingSenderId: "607309279287",
-  appId: "1:607309279287:web:6aeee8ced61247971c4fb2",
-  measurementId: "G-F12RDTHC71",
+  apiKey: "AIzaSyBRJn3K3Ny2Gr4D17ECEhnOWRDY6zt9iI4",
+  authDomain: "shopproject-42038.firebaseapp.com",
+  projectId: "shopproject-42038",
+  storageBucket: "shopproject-42038.appspot.com",
+  messagingSenderId: "863671062765",
+  appId: "1:863671062765:web:d4829bd20150c6a1a33ce4",
+  measurementId: "G-2JQW2EHLRM",
 };
 
 initializeApp(firebaseConfig);
