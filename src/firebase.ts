66f553dacc7f054995e128c6cb059ef4d8e2c762
mvoloshyn.appwell <mvoloshyn.appwell@ancestry.com>
2022-03-15 22:29:38 +0200

    changed

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRJn3K3Ny2Gr4D17ECEhnOWRDY6zt9iI4",
-  authDomain: "shopproject-42038.firebaseapp.com",
-  projectId: "shopproject-42038",
-  storageBucket: "shopproject-42038.appspot.com",
-  messagingSenderId: "863671062765",
-  appId: "1:863671062765:web:d4829bd20150c6a1a33ce4",
-  measurementId: "G-2JQW2EHLRM",
+  apiKey: "AIzaSyCBpG4MHOqzVbyDdrF8kf4QqQ_U1wciGnk",
+  authDomain: "shopproject-2058d.firebaseapp.com",
+  projectId: "shopproject-2058d",
+  storageBucket: "shopproject-2058d.appspot.com",
+  messagingSenderId: "828530404628",
+  appId: "1:828530404628:web:fd8b12494e77fc6fc3a601",
+  measurementId: "G-HMPGCNYYR5"
 };
 
 initializeApp(firebaseConfig);
